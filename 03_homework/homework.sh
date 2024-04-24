3abd3c77f5ab514abe3f6ca5f8bcc50ad43458d1
AndrianaO <79010578+AndrianaO@users.noreply.github.com>
2024-04-24 16:52:11 -0400

    Update homework.sh

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -8,20 +8,42 @@
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
 
+$ mkdir dir{1..5}
+
 # 2. How would you verify the creation of all 5 directories?
+
+$ ls
 
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
 
+$ touch ./dir{1..5}/message.txt
+$ echo -n 'I love data' > find -type f
+
 # 4. How would you verify the presence of all 5 files?
+
+$ find -type f
 
 # 5. How would you append to one of the existing files " and machine learning!"?
 
+$ echo ' and machine learning!' >> ./dir1/message.txt
+
 # 6. How would you verify that the text was indeed appended to the existing file?
+
+$ cat ./dir1/message.txt
 
 # 7. How would you delete all files except for the one with the appended text?
 
+$ rm dir{2..5}/*.txt
+
 # 8. How would you navigate back to the parent directory containing all the directories?
+
+# I am already in that directory, but to navigate to parent one one should use the following:
+$ cd ..
 
 # 9. How would you remove each directory along with its contents?
 
+$ rm -r dir{1..5}
+
 # 10. How would you verify that all directories and files have been deleted?
+
+$ ls
